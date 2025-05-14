730ad62aa5086cafb6f15e69cb977c9b374b2a37
Arthur Lovekin <arthur.lovekin@scaledfoundations.ai>
2025-05-14 15:39:01 -0700

    namespace in pubsub example

--- pubsub_subscriber.py
+++ pubsub_subscriber.py
@@ -1,5 +1,11 @@
 import zenoh, time
 from zenoh.ext import z_deserialize, Float64
+
+"""
+Shows how to use the zenoh serializer (as opposed to json) to subscribe to floats and strings.
+Also shows how to set a namespace
+"""
+
 def print_sample_attributes(sample):
     attrs = [attr for attr in dir(sample) if not attr.startswith("__") and not callable(getattr(sample, attr))]
     print("Available sample attributes:")
@@ -20,7 +26,10 @@ def float_listener(sample):
     # print_sample_attributes(sample)
 
 if __name__ == "__main__":
-    with zenoh.open(zenoh.Config()) as session:
+    config = zenoh.Config()
+    config.insert_json5("namespace", '\"my_namespace\"')
+
+    with zenoh.open(config) as session:
         temp_sub = session.declare_subscriber('myhome/kitchen/temp', string_listener)
         time_sub = session.declare_subscriber('myhome/kitchen/time', float_listener)
         vel_sub = session.declare_subscriber('cmd_vel', string_listener)
